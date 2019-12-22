77fa4822507a4a8befd028da7eecf5bb215f4898
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-21 21:49:06 -0300

    show consumption converter currency

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -43,10 +43,36 @@ class ViewController: UIViewController {
                 // 2
                 if data != nil {
                     do{
-                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
+                        
                         // ASYNC
                         DispatchQueue.main.async {
-                            print(jsonResponse)
+                            if let rates = jsonResponse["rates"] as? [String: Any] {
+                                //print(rates)
+                                if let cad = rates["CAD"] as? Double {
+                                     self.cadLabel.text = "CAD: \(cad)"
+                                }
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text = "CHF: \(chf)"
+                                }
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.cbpLabel.text = "GBP: \(gbp)"
+                                }
+                                if let jpy = rates["JPY"] as? Double {
+                                   self.jpyLabel.text = "JPY: \(jpy)"
+                                }
+                                if let usd = rates["USD"] as? Double {
+                                   self.usdLabel.text = "USD: \(usd)"
+                                }
+                                if let turkish = rates["TRY"] as? Double {
+                                   self.tryLabel.text = "TRY: \(turkish)"
+                                }
+                                if let brl = rates["BRL"] as? Double {
+                                   self.brlLabel.text = "BRL: \(brl)"
+                                }
+                                
+                            }
+                            //print(jsonResponse["rates"])
                         }
                         
                     }catch{
